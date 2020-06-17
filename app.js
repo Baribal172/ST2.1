10e91ad785e1587b04228a45e9768c266a8b9463
Baribal172 <vincentvandesompel@hotmail.com>
2020-06-17 12:20:46 +0200

    Update app.js automaticly assign port

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/gmail', gmailRouter);
-
+app.listen(process.env.PORT || 5000);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
